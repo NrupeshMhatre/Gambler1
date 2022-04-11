98477f09366d2aad6b3107430614e7ac22341de8
NrupeshMhatre <100428751+NrupeshMhatre@users.noreply.github.com>
2022-04-11 22:06:34 +0530

    Gambler Win or Loose

--- src/gambler/Gambler.java
+++ src/gambler/Gambler.java
@@ -4,8 +4,19 @@ public class Gambler {
         static int stake = 100;
 	static int bet = 1;
 	public static void main(String[] args) {
-		System.out.print("hello gambler");
+	  System.out.println("available Stack :" + stake + "$");
+	  System.out.println("Person Betting Every Game :" + bet + "$");
+	   int betPrice = (int) Math.floor(Math.random() * 10) % 2;
+	    	  if (betPrice == 1) 
+		{
+	           System.out.println("Won then Get :" + betPrice + "$");	   
+			 } else {
+		    System.out.println("loose then Get:" + betPrice + "$");
+	     		   }
+             
+		}
+
 
 	}
 
-}
+
