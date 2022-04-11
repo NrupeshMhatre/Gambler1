b505023f20b15331c9fcb3aa7933fd9e562fdba2
NrupeshMhatre <100428751+NrupeshMhatre@users.noreply.github.com>
2022-04-11 22:14:11 +0530

    After 20 Days Win or Lost

--- src/gambler/Gambler.java
+++ src/gambler/Gambler.java
@@ -3,13 +3,15 @@ package gambler;
 public class Gambler {
         
 	public static void main(String[] args) {
-	  int stake = 100;
-	  int bet = 1;
-	    System.out.println("Person Having Stack :" + stake + "$");
-	    System.out.println("Person Betting Every Game :" + bet + "$");
-		while (stake <= 150 && stake >= 50) {
-	        int betPrice = (int) Math.floor(Math.random() * 10) % 2;
-		  if (betPrice == 1) {
+	int stake = 100;
+	int bet = 1;
+	int TotalwinAmt = 0;
+        System.out.println("Person Having Stack :" + stake + "$");
+        System.out.println("Person Betting Every Game :" + bet + "$");
+	  for(int day=1;day<=20;day++) {
+	   while (stake <= 150 && stake >= 50) {
+		 int betPrice = (int) Math.floor(Math.random() * 10) % 2;
+		    if (betPrice == 1) {
 			System.out.println("Won He Get :" + betPrice + "$");
 			stake++;
 			System.out.println("Total stake :" + stake);
@@ -17,15 +19,17 @@ public class Gambler {
 			System.out.println("loose He Get:" + betPrice + "$");
 			stake--;
 			System.out.println("Total stake :" + stake);
-	            }
-	        }
-	        int TotalStake = stake;
-	        System.out.println("Total Stake For Resign A Day is :" + TotalStake);
-	    }
+				    }
+		        }
+			int day_win_amount = stake - 100;
+			System.out.println(" Amount won for day " + day + " is " + day_win_amount);
+			TotalwinAmt = TotalwinAmt + day_win_amount;
+			System.out.println("total amount " + TotalwinAmt);
+			}  
 	     		   }
              
 		}
-
+}
 
 	
 
