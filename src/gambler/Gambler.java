af57112d9a0ef5fae04f5a9ef81b838e758a509f
NrupeshMhatre <100428751+NrupeshMhatre@users.noreply.github.com>
2022-04-11 22:02:36 +0530

    Initializing Variables

--- src/gambler/Gambler.java
+++ src/gambler/Gambler.java
@@ -1,7 +1,8 @@
 package gambler;
 
 public class Gambler {
-
+        static int stake = 100;
+	static int bet = 1;
 	public static void main(String[] args) {
 		System.out.print("hello gambler");
 
